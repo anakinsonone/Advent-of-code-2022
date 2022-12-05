21aa66d07372972788312c1a281e953069fe4c8b
anakinsonone <akkhi.sonone@gmail.com>
2022-12-05 18:37:45 +0530

    solution for day 3 part 2

--- day 3/solution.py
+++ day 3/solution.py
@@ -1,13 +1,13 @@
 import string
+import itertools
 
 #open input file
-inputFile = open('input.txt')    
+inputFile = open('input.txt')   
 
-#split every line into a string
-itemList = inputFile.read().split()
-
+#create a list of alphabets
 alphabets = list(string.ascii_letters)
 
+#create dictionary
 class myDictionary(dict):
     def __init__(self):
         self = dict()
@@ -15,20 +15,47 @@ class myDictionary(dict):
     def add(self, key, value):
         self[key] = value
 
+#create an object of the dictionary
 dictObj = myDictionary()
 
+#add priorities to each alphabet
 for i in range(len(alphabets)):
     dictObj.key = alphabets[i]
     dictObj.value = i+1
     dictObj.add(dictObj.key, dictObj.value)
 
+#split every line into a string
+itemList = inputFile.read().split()
+
+#part 1
+
+#define splices to divide the string into 2 equal parts and find the common char
 count = 0
-#define splices in order to divide the string into 2 equal parts and find the common char
 for item in range(len(itemList)):
     string1 = slice(0, len(itemList[item])//2)
     string2 = slice(len(itemList[item])//2, len(itemList[item]))
     commonChar = ''.join(set(itemList[item][string1]).intersection(itemList[item][string2]))
     count = count + dictObj.get(commonChar)
 
-print(count)
+print('Sum of all priorities:', count)
+
+#part 2
+
+#define a function to create groups of three
+def grouper(n, iterable):
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args)
+
+groups = list(grouper(3, itemList))
+
+badge = 0
+#compare strings within each group and find the common item
+for string in range(len(groups)):
+    string1 = groups[string][0]
+    string2 = groups[string][1]
+    string3 = groups[string][2]
+    commonItem = ''.join(set(string1).intersection(string2).intersection(string3))
+    badge = badge + dictObj.get(commonItem)
+
+print("Sum of common badge priorities:", badge)
 inputFile.close()
